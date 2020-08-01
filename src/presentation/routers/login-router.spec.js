1ac940efda537e96e11556dbde31658efab72937
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-01 19:42:31 -0300

    ensure returns 401 if invalid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,12 +2,23 @@ const LoginRouter = require('./login-routes')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCaseSpy {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+  const authUseCaseSpy = new AuthUseCaseSpy()
+  const sut = new LoginRouter(authUseCaseSpy)
+  return {
+    sut,
+    authUseCaseSpy
+  }
 }
 
 describe('Login route', () => {
   test('Should return 400 if email is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'aaa'
@@ -19,7 +30,7 @@ describe('Login route', () => {
   })
 
   test('Should return 400 if password is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'aaa'
@@ -31,21 +42,40 @@ describe('Login route', () => {
   })
 
   test('Should return 400 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpRequest is not provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 
-  test('Should return 500 if httpRequest is not provided', () => {
-    const sut = makeSut()
-    const httpResponse = sut.route()
-    expect(httpResponse.statusCode).toBe(500)
+  test('Should call AuthUseCaseSpy with right params', () => {
+    const { sut, authUseCaseSpy } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any',
+        password: 'any'
+      }
+    }
+    sut.route(httpRequest)
+    expect(authUseCaseSpy.email).toBe(httpRequest.body.email)
+    expect(authUseCaseSpy.password).toBe(httpRequest.body.password)
+  })
+
+  test('Should return 401 when invalid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_invalid',
+        password: 'any_invalid'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(401)
   })
 })
--- src/presentation/routers/login-routes.js
+++ src/presentation/routers/login-routes.js
@@ -1,14 +1,24 @@
 const HttpResponse = require('../helpers/http-response')
 
 module.exports = class LoginRouter {
+  constructor (authUseCase) {
+    this.authUseCase = authUseCase
+  }
+
   route (httpRequest) {
     if (httpRequest && httpRequest.body) {
       const { email, password } = httpRequest.body
+
       if (!email) {
         return HttpResponse.badRequest('email')
       }
+
       if (!password) {
         return HttpResponse.badRequest('password')
+      }
+      this.authUseCase.auth(email, password)
+      return {
+        statusCode: 401
       }
     } else {
       return HttpResponse.serverError()
