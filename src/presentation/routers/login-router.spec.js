12213e789c61c54df21597c8e3cc7bb2510808a5
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-01 17:26:36 -0300

    making custom erros for badRequest erros

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -2,16 +2,37 @@ class LoginRouter {
   route (httpRequest) {
     if (httpRequest && httpRequest.body) {
       const { email, password } = httpRequest.body
-      if (!email || !password) {
-        return {
-          statusCode: 400
-        }
+      if (!email) {
+        return HttpResponse.badRequest('email')
+      }
+      if (!password) {
+        return HttpResponse.badRequest('password')
       }
     } else {
-      return {
-        statusCode: 500
-      }
+      return HttpResponse.serverError()
     }
+  }
+}
+
+class HttpResponse {
+  static badRequest (errorName) {
+    return {
+      statusCode: 400,
+      body: new MissingParamError(errorName)
+    }
+  }
+
+  static serverError () {
+    return {
+      statusCode: 500
+    }
+  }
+}
+
+class MissingParamError extends Error {
+  constructor (paramName) {
+    super(`Missing param: ${paramName}`)
+    this.name = 'MissingParamError'
   }
 }
 
@@ -25,6 +46,7 @@ describe('Login route', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new MissingParamError('email'))
   })
   test('Should return 400 if password is not provided', () => {
     const sut = new LoginRouter()
@@ -35,16 +57,17 @@ describe('Login route', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
-  })
-  test('Should return 500 if httpRequest is not provided', () => {
-    const sut = new LoginRouter()
-    const httpResponse = sut.route()
-    expect(httpResponse.statusCode).toBe(500)
+    expect(httpResponse.body).toEqual(new MissingParamError('password'))
   })
   test('Should return 400 if httpRequest has no body', () => {
     const sut = new LoginRouter()
     const httpRequest = {}
     const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(500)
+  })
+  test('Should return 500 if httpRequest is not provided', () => {
+    const sut = new LoginRouter()
+    const httpResponse = sut.route()
     expect(httpResponse.statusCode).toBe(500)
   })
 })
