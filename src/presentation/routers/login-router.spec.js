2f1b5c6250cadc649a8693ac1d9d7ea271e99e9d
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-01 16:48:42 -0300

    test if password isnot provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,7 +1,7 @@
 class LoginRouter {
   route (httpRequest) {
-    const { email } = httpRequest.body
-    if (!email) {
+    const { email, password } = httpRequest.body
+    if (!email || !password) {
       return {
         statusCode: 400
       }
@@ -15,6 +15,16 @@ describe('Login route', () => {
     const httpRequest = {
       body: {
         password: 'aaa'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+  test('Should return 400 if password is not provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'aaa'
       }
     }
     const httpResponse = sut.route(httpRequest)
