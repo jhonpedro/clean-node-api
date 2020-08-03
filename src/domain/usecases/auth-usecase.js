f6d444ef6c6da0914204e89f5ca9b0d460f04b2b
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-03 16:06:52 -0300

    removing try catch

--- src/domain/usecases/auth-usecase.js
+++ src/domain/usecases/auth-usecase.js
@@ -1,4 +1,4 @@
-const { MissingParamError, InvalidParamError } = require('../../utils/errors')
+const { MissingParamError } = require('../../utils/errors')
 
 module.exports = class AuthUseCase {
   constructor ({ loadUserByEmailRepository, encrypter, tokenGenerator } = {}) {
@@ -8,26 +8,21 @@ module.exports = class AuthUseCase {
   }
 
   async auth (email, password) {
-    try {
-      if (!email) {
-        throw new MissingParamError('email')
-      }
-      if (!password) {
-        throw new MissingParamError('password')
-      }
-      const user = await this.loadUserByEmailRepository.load(email)
-      if (user) {
-        const isValid = await this.encrypterSpy.compare(password, user.password)
-        if (isValid) {
-          const acessToken = await this.tokenGeneratorSpy.generate(user.id)
-          return acessToken
-        }
-      }
-
-      return null
-    } catch (error) {
-      console.log(error.message)
-      throw new InvalidParamError(error.message)
+    if (!email) {
+      throw new MissingParamError('email')
     }
+    if (!password) {
+      throw new MissingParamError('password')
+    }
+    const user = await this.loadUserByEmailRepository.load(email)
+    if (user) {
+      const isValid = await this.encrypterSpy.compare(password, user.password)
+      if (isValid) {
+        const acessToken = await this.tokenGeneratorSpy.generate(user.id)
+        return acessToken
+      }
+    }
+
+    return null
   }
 }
