e6ac4f6755bebd07b6f7a5a4fa3a2ddebf48d66d
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-04 11:51:42 -0300

    adding jest-mongodb for tests and ensuring LoadUserByEmailRepo returns correct values

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,6 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',
-  collectCoverageFrom: ['**/src/**/*.js']
+  collectCoverageFrom: ['**/src/**/*.js'],
+  preset: '@shelf/jest-mongodb'
 }
--- src/infra/repositores/load-user-by-email-repository.spec.js
+++ src/infra/repositores/load-user-by-email-repository.spec.js
@@ -1,22 +1,60 @@
+const { MongoClient } = require('mongodb')
+
 class LoadUserByEmailRepository {
+  constructor (userModel) {
+    this.userModel = userModel
+  }
+
   load (email) {
-    return null
+    const user = this.userModel.findOne({ email })
+    return user
   }
 }
 
-const makeSut = () => {
-  const loadUserByEmailRepository = new LoadUserByEmailRepository()
+// const makeSut = () => {
+//   const loadUserByEmailRepository = new LoadUserByEmailRepository()
 
-  return {
-    sut: loadUserByEmailRepository
-  }
-}
+//   return {
+//     sut: loadUserByEmailRepository
+//   }
+// }
 
 describe('Load user by email repository', () => {
+  let connection
+  let db
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    db = await connection.db()
+  })
+
+  beforeEach(async () => {
+    await db.collection('users').deleteMany()
+  })
+  afterAll(async () => {
+    await connection.close()
+  })
+
   test('Should return null if repository returns null', async () => {
-    const { sut } = makeSut()
+    const userModel = db.collection('users')
+    const sut = new LoadUserByEmailRepository(userModel)
 
     const user = await sut.load('invalid@email.com')
     expect(user).toBeNull()
+  })
+
+  test('Should return user if repository returns a user', async () => {
+    const email = 'valid@email.com'
+    const userModel = db.collection('users')
+    userModel.insertOne({
+      email
+    })
+    const sut = new LoadUserByEmailRepository(userModel)
+
+    const user = await sut.load(email)
+    expect(user.email).toBe(email)
   })
 })
