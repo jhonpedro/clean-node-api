2f390095a25bf28065572eb568981556f082b8f9
Joao Pedro <pedrobarros2010@gmail.com>
2020-08-04 10:15:48 -0300

    adding jwt and ensuring token generator return correct values

--- src/utils/helpers/token-generator.spec.js
+++ src/utils/helpers/token-generator.spec.js
@@ -1,6 +1,10 @@
+const JWT = require('jsonwebtoken')
+
 class TokenGenerator {
   async generate (id) {
-    return null
+    const token = JWT.sign(id, 'secret')
+    this.token = token
+    return token
   }
 }
 const makeSut = () => {
@@ -14,8 +18,16 @@ const makeSut = () => {
 describe('Token Generator', () => {
   test('Should return null if token generetor returns null', async () => {
     const { sut } = makeSut()
+    JWT.token = null
 
     const token = await sut.generate('any_id')
     expect(token).toBe(null)
+  })
+
+  test('Should return a token if jwt returns a token', async () => {
+    const { sut } = makeSut()
+
+    const token = await sut.generate('any_id')
+    expect(token).toBe(sut.token)
   })
 })
